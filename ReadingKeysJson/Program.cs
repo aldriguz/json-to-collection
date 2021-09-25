9c2d15ba962fa1bc8592b5c0194825d847b6678e
Kevin Alama <alama1992@gmail.com>
2021-09-25 14:28:56 -0500

    Add project files.

---
+++ ReadingKeysJson/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ReadingKeysJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> keyValueMap = new Dictionary<string, string>();
+            
+            using (StreamReader reader = File.OpenText(@"C:\Users\Kevin\data.txt")) 
+            {
+                var fileContent = reader.ReadToEnd();
+
+                var data = (JObject)JsonConvert.DeserializeObject(fileContent);
+                string _rates = JsonConvert.SerializeObject(data["rates"]);
+
+                JObject converted = JsonConvert.DeserializeObject<JObject>(_rates);
+
+                if (converted != null)
+                {
+                    foreach (KeyValuePair<string, JToken> keyValuePair in converted)
+                    {
+                        keyValueMap.Add(keyValuePair.Key, keyValuePair.Value.ToString());
+                        Console.WriteLine(keyValuePair.Key);
+                    }
+                }
+            }            
+        }
+
+    }
+}
